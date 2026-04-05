398e6b95a181572fef7c109b5c29c5b1cbeb38cb
alopezinteca-dotcom <alopezinteca@gmail.com>
2026-04-05 11:23:10 +0200

    Create top_menu.dart

---
+++ lib/widgets/top_menu.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class TopMenu extends StatelessWidget {
+  final VoidCallback onRealLocation;
+  final VoidCallback onToggleSat;
+  final bool isSatellite;
+  final VoidCallback onPhoto;
+  final VoidCallback onGallery;
+  final VoidCallback onSettings;
+
+  const TopMenu({
+    super.key,
+    required this.onRealLocation,
+    required this.onToggleSat,
+    required this.isSatellite,
+    required this.onPhoto,
+    required this.onGallery,
+    required this.onSettings,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+      decoration: BoxDecoration(
+        color: Colors.blueGrey[900]?.withOpacity(0.9),
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: const [
+          BoxShadow(color: Colors.black45, blurRadius: 6)
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          IconButton(
+            icon: const Icon(Icons.my_location, color: Colors.white),
+            onPressed: onRealLocation,
+          ),
+          Container(width: 1, height: 30, color: Colors.grey),
+
+          IconButton(
+            icon: Icon(
+              isSatellite ? Icons.map : Icons.satellite_alt,
+              color: Colors.white,
+            ),
+            onPressed: onToggleSat,
+          ),
+          Container(width: 1, height: 30, color: Colors.grey),
+
+          IconButton(
+            icon: const Icon(Icons.camera_alt, color: Colors.white),
+            onPressed: onPhoto,
+          ),
+          Container(width: 1, height: 30, color: Colors.grey),
+
+          IconButton(
+            icon: const Icon(Icons.photo_library, color: Colors.white),
+            onPressed: onGallery,
+          ),
+          Container(width: 1, height: 30, color: Colors.grey),
+
+          IconButton(
+            icon: const Icon(Icons.settings, color: Colors.white),
+            onPressed: onSettings,
+          ),
+        ],
+      ),
+    );
+  }
+}
+``
