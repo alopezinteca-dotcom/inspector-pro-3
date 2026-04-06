205d24dd98ec3c747f7af81b42252da238c31b13
alopezinteca-dotcom <alopezinteca@gmail.com>
2026-04-06 10:45:50 +0200

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -5,7 +5,6 @@ pluginManagement {
         gradlePluginPortal()
     }
 
-    // ✅ Cargar path del SDK de Flutter
     val flutterSdkPath = run {
         val properties = java.util.Properties()
         file("local.properties").inputStream().use { properties.load(it) }
@@ -14,18 +13,16 @@ pluginManagement {
         path
     }
 
-    // ✅ ENLAZAR GRADLE DE FLUTTER (CLAVE ABSOLUTA)
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 }
 
 plugins {
-    // ✅ ESTE SÍ VA AQUÍ, y SOLO este
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
 
-    // ✅ AGP y Kotlin declarados SOLO PARA QUE EXISTAN, pero sin aplicar
-    id("com.android.application") version "8.6.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.android.application") version "8.9.1" apply false
+    id("com.android.library") version "8.9.1" apply false
+
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
 }
 
-// ✅ Registrar el módulo app
 include(":app")
